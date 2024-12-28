b1d195b46933142d2793c4291ff7b226767d1d49
Petar Lenart <petar.lenart@gmail.com>
2024-12-28 15:30:08 +0100

    refactored navigation to be contain routes ,added check if user is logged in when opening the app and navigation to home screen, added bottom navigation bar

--- app/src/main/java/com/plenart/emotionstationcompose/navigation/ESRoutes.kt
+++ app/src/main/java/com/plenart/emotionstationcompose/navigation/ESRoutes.kt
@@ -1,9 +1,53 @@
 package com.plenart.emotionstationcompose.navigation
 
-import kotlinx.serialization.Serializable
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.outlined.Face
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Info
+import androidx.compose.ui.graphics.vector.ImageVector
+import com.plenart.emotionstationcompose.R
 
-@Serializable object SignInScreen
+const val SIGN_IN_ROUTE = "sign_in"
+const val SIGN_UP_ROUTE = "sign_up"
+const val HOME_ROUTE = "home"
+const val CHILDREN_ROUTE = "children"
+const val INFO_ROUTE = "info"
 
-@Serializable object SignUpScreen
+sealed class EmotionStationDestination(open val route: String)
 
-@Serializable object HomeScreen
+sealed class ESRoute(
+    override val route: String,
+    val iconUnselected: ImageVector? = null,
+    val iconSelected: ImageVector? = null,
+    val labelId: Int? = null,
+) : EmotionStationDestination(route) {
+
+    data object HomeScreen :
+        ESRoute(
+            HOME_ROUTE,
+            iconSelected = Icons.Default.Home,
+            iconUnselected = Icons.Outlined.Home,
+            labelId = R.string.navigation_bar_home_screen_parent,
+        )
+
+    data object ChildrenScreen :
+        ESRoute(
+            CHILDREN_ROUTE,
+            iconSelected = Icons.Default.Face,
+            iconUnselected = Icons.Outlined.Face,
+            labelId = R.string.navigation_bar_children_screen,
+        )
+
+    data object InfoScreen : ESRoute(
+        INFO_ROUTE,
+        iconSelected = Icons.Default.Info,
+        iconUnselected = Icons.Outlined.Info,
+        labelId = R.string.navigation_bar_info_screen,
+    )
+
+    data object SignInScreen : ESRoute(SIGN_IN_ROUTE)
+    data object SignUpScreen : ESRoute(SIGN_UP_ROUTE)
+}
--- app/src/main/java/com/plenart/emotionstationcompose/ui/main/MainScreen.kt
+++ app/src/main/java/com/plenart/emotionstationcompose/ui/main/MainScreen.kt
@@ -1,45 +1,93 @@
 package com.plenart.emotionstationcompose.ui.main
 
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.navigation.NavDestination
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import com.plenart.emotionstationcompose.navigation.HomeScreen
-import com.plenart.emotionstationcompose.navigation.SignInScreen
-import com.plenart.emotionstationcompose.navigation.SignUpScreen
+import com.plenart.emotionstationcompose.data.authentication.AuthenticationRepository
+import com.plenart.emotionstationcompose.navigation.ESRoute
 import com.plenart.emotionstationcompose.ui.authentication.signIn.SignInScreen
 import com.plenart.emotionstationcompose.ui.authentication.signIn.SignInViewModel
 import com.plenart.emotionstationcompose.ui.authentication.signUp.SignUpScreen
+import com.plenart.emotionstationcompose.ui.children.ChildrenScreen
 import com.plenart.emotionstationcompose.ui.home.HomeScreen
+import com.plenart.emotionstationcompose.ui.info.InfoScreen
 import org.koin.androidx.compose.koinViewModel
+import org.koin.compose.koinInject
 
 @Composable
 fun MainScreen() {
     val navController = rememberNavController()
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
 
-    Scaffold { padding ->
+    val showBottomNavigation by remember {
+        derivedStateOf {
+            when (navBackStackEntry?.destination?.route) {
+                ESRoute.SignInScreen.route,
+                ESRoute.SignUpScreen.route -> false
+
+                else -> true
+            }
+        }
+    }
+
+    Scaffold(
+        bottomBar = {
+            if (showBottomNavigation) {
+                BottomNavigationBar(
+                    destinations = listOf(
+                        ESRoute.HomeScreen,
+                        ESRoute.ChildrenScreen,
+                        ESRoute.InfoScreen
+                    ),
+                    currentDestination = navBackStackEntry?.destination,
+                    onNavigateToDestination = { dest ->
+                        navController.navigate(dest.route) {
+                            this.popUpTo(
+                                route = dest.route,
+                                popUpToBuilder = {
+                                    inclusive = true
+                                },
+                            )
+                        }
+                    }
+                )
+            }
+        }
+    ) { padding ->
         Surface(
             color = MaterialTheme.colorScheme.background,
             modifier = Modifier.padding(padding)
         ) {
             NavHost(
                 navController = navController,
-                startDestination = SignUpScreen
+                startDestination = ESRoute.SignUpScreen.route,
             ) {
-                composable<SignInScreen> {
+                composable(ESRoute.SignInScreen.route) {
                     val viewModel = koinViewModel<SignInViewModel>()
                     val state = viewModel.state.collectAsState()
 
                     LaunchedEffect(key1 = state.value.isSignInSuccessful) {
                         if (state.value.isSignInSuccessful) {
-                            navController.navigate(HomeScreen)
+                            navController.navigate(ESRoute.HomeScreen.route)
                             viewModel.resetState()
                         }
                     }
@@ -48,17 +96,27 @@ fun MainScreen() {
                         onSignInAction = viewModel::signIn,
                         onPasswordChange = viewModel::onPasswordChange,
                         onEmailChange = viewModel::onEmailChange,
-                        onNavigateToSignUpScreen = { navController.navigate(SignUpScreen) },
+                        onNavigateToSignUpScreen = { navController.navigate(ESRoute.SignUpScreen.route) },
                     )
+
                 }
-                composable<SignUpScreen> {
+                composable(ESRoute.SignUpScreen.route)
+                {
+                    val authenticationRepository = koinInject<AuthenticationRepository>()
+
+                    LaunchedEffect(key1 = Unit) {
+                        if (authenticationRepository.hasUser()) {
+                            navController.navigate(ESRoute.HomeScreen.route)
+                        }
+                    }
+
                     SignUpScreen(
                         onNavigateToSignIn = {
-                            navController.navigate(SignInScreen)
+                            navController.navigate(ESRoute.SignInScreen.route)
                         },
                     )
                 }
-                composable<HomeScreen> {
+                composable(ESRoute.HomeScreen.route) {
                     val viewmodel = koinViewModel<SignInViewModel>()    //Temporary
 
                     HomeScreen(
@@ -68,9 +126,45 @@ fun MainScreen() {
                         },
                     )
                 }
+                composable(ESRoute.ChildrenScreen.route) {
+                    ChildrenScreen()
+                }
+                composable(ESRoute.InfoScreen.route) {
+                    InfoScreen()
+                }
             }
         }
     }
 }
 
+@Composable
+fun BottomNavigationBar(
+    destinations: List<ESRoute>,
+    onNavigateToDestination: (ESRoute) -> Unit,
+    currentDestination: NavDestination?,
+) {
+    NavigationBar {
+        destinations.forEach { dest ->
+            val selected = currentDestination?.route == dest.route
+            NavigationBarItem(
+                selected = selected,
+                label = {
+                    Text(
+                        stringResource(id = dest.labelId!!),
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
+                    )
+                },
+                icon = {
+                    Icon(
+                        imageVector =
+                        if (selected) dest.iconSelected!! else dest.iconUnselected!!,
+                        contentDescription = null
+                    )
+                },
+                onClick = { onNavigateToDestination(dest) }
+            )
+        }
 
+    }
+}
